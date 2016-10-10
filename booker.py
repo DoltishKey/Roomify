5a626397e7ebb6cffe86e46404720a859a8e4538
DoltishKey <jcbpettersson@gmail.com>
2016-10-10 12:33:51 +0200

    1.0
    Nystart av projektet

---
+++ booker.py
@@ -0,0 +1,17 @@
+
+import requests
+#from bs4 import BeautifulSoup
+
+data = {
+    'username': 'AC8240',
+    'password': '92F39gb2'
+}
+
+head = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}
+with requests.session() as s:
+    resp = s.get('https://schema.mah.se')
+    resp = s.post('https://schema.mah.se/login_do.jsp', data=data, headers=head)
+    resp = s.get('https://schema.mah.se/resursbokning.jsp?flik=FLIK-0017')
+
+
+    print resp.content
